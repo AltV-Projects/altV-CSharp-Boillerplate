2c2b57726a685f2fcd22870b58d9c53d2850c6de
Franze, Mario <mario.franze@gmail.com>
2022-04-19 00:02:41 +0200

    Added some example scripts

---
+++ altV-Boillerplate/Server-Core/Events/BuiltIn/PlayerDisconnect.cs
@@ -0,0 +1,18 @@
+﻿using AltV.Net;
+using altV_Boillerplate.Entities;
+
+namespace altV_Boillerplate.Events.BuiltIn
+{
+    internal class PlayerDisconnect : IScript
+    {
+        [ScriptEvent(ScriptEventType.PlayerDisconnect)]
+        public void OnPlayerDisconnect(XPlayer player, string reason)
+        {
+            if (!player.IsLoggedIn)
+            {
+                Alt.Log($"Could not save player {player.Name} because he isn't logged in.");
+            }
+            Alt.Log($"Player {player.Name} disconnected to our server with reason: {reason}");
+        }
+    }
+}
---
+++ altV-Boillerplate/Server-Core/Events/BuiltIn/WeaponDamage.cs
@@ -0,0 +1,18 @@
+﻿using AltV.Net;
+using AltV.Net.Data;
+using AltV.Net.Elements.Entities;
+
+namespace altV_Boillerplate.Events.BuiltIn
+{
+    internal class WeaponDamage : IScript
+    {
+        [ScriptEvent(ScriptEventType.WeaponDamage)]
+        public static bool OnWeaponDamage(IPlayer victim, IEntity attacker, uint weapon, ushort damage, Position offset, BodyPart bodyPart)
+        {
+            // Place your logic here
+
+            // Return false, to cancel weapon damage
+            return true;
+        }
+    }
+}
