52ea99717da591255fd34f99775a549defb0deba
Franze, Mario <mario.franze@gmail.com>
2022-04-19 00:03:01 +0200

    Added entity factory

---
+++ altV-Boillerplate/Server-Core/Entities/XPlayerFactory.cs
@@ -0,0 +1,14 @@
+﻿using AltV.Net;
+using AltV.Net.Elements.Entities;
+
+namespace altV_Boillerplate.Entities
+{
+    internal class XPlayerFactory : IEntityFactory<IPlayer>
+    {
+        public IPlayer Create(ICore server, IntPtr playerPointer, ushort id)
+        {
+            return new XPlayer(playerPointer, id);
+        }
+
+    }
+}
