eacbd48ff2be8679afb65686d6c46fe76560c8a9
Franze, Mario <mario.franze@gmail.com>
2022-04-19 07:04:22 +0200

    Added start stop behaviour

---
+++ altV-Boillerplate/Client-Core/Events/Builtin/StartStop.cs
@@ -0,0 +1,17 @@
+﻿using AltV.Net.Client;
+
+namespace Client_Core.Events.Builtin
+{
+    internal class StartStop : Resource
+    {
+        public override void OnStart()
+        {
+            Alt.LogInfo("Client Started!");
+        }
+
+        public override void OnStop()
+        {
+            Alt.LogInfo("Client Stopped!");
+        }
+    }
+}
