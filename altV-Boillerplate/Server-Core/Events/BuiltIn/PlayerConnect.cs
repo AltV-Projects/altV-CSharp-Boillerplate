0349f7879d3120e07804e9a963c9f3f31e198ab3
Franze, Mario <mario.franze@gmail.com>
2022-04-19 23:10:14 +0200

    Added basic check, if the user has an account

--- altV-Boillerplate/Server-Core/Events/BuiltIn/PlayerConnect.cs
+++ altV-Boillerplate/Server-Core/Events/BuiltIn/PlayerConnect.cs
@@ -13,10 +13,18 @@ namespace altV_Boillerplate.Events.BuiltIn
             player.Model = (uint)PedModel.FreemodeMale01; // Set to default model
             player.Spawn(new(0, 0, 72), 1000); // Set to default spawn
             Alt.Log($"Player {player.Name} connected to our server.");
+
             if (player.DoesHaveAccount())
+            {
                 Alt.Log($"{player.Name} has a record in the database.");
+                player.Login("dummy@mail.com");
+            }
             else
+            {
                 Alt.Log($"{player.Name} has no record in the database.");
+                player.Register("dummy@mail.com");
+            }
+            Alt.Log($"Set player.DBID to {player.DBID}");
         }
     }
 }
